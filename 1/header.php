87993c4d6a9a889fe257994d4a31d8c3dbe05bc9
H <heshamscorpe55@gmail>
2016-03-29 16:40:44 +0200

    MVC 1

---
+++ 1/header.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>CS50</title>
+
+		<body>
+			
---
+++ 1/index.php
@@ -0,0 +1,9 @@
+<?php require("header.php") ?>
+
+<ul>
+	<li><a href="#">lectures</a></li>
+	<li><a href="#">sections</a></li>
+</ul>
+
+
+<?php require("footer.php") ?>
