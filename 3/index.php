10c6817fc506e31ce4ee6603d60691122c62acd2
H <heshamscorpe55@gmail>
2016-03-29 17:05:07 +0200

    MVC 3

---
+++ 3/index.php
@@ -0,0 +1,10 @@
+<?php require("helpers.php"); ?>
+
+<?php render("header", ["title" => "CS50"]); ?>
+
+	<ul>
+		<li><a href="#">Lectures</a></li>
+		<li><a href="#">Sections</a></li>
+	</ul>
+
+<?php render("footer"); ?>
